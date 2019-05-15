ee0b93070d2e2bcb6bad65b15eb26aca705c954c
Wojciech Wajerowicz <wojciech.wajerowicz@epfl.ch>
2019-05-15 16:08:18 +0200

    Update version number.

--- nexussdk/utils/tools.py
+++ nexussdk/utils/tools.py
@@ -1,6 +1,8 @@
 import copy
 import json
 
+from typing import Optional
+
 
 def copy_this_into_that(this, that, deepcopy=False, force=False):
     """
@@ -28,3 +30,33 @@ def pretty_print(payload):
     :param payload: A Python dict
     """
     print(json.dumps(payload, indent=2))
+
+
+def listing_params(pagination_from: Optional[int], pagination_size: Optional[int],
+                    deprecated: Optional[bool], type: Optional[str], created_by: Optional[str],
+                    updated_by: Optional[str], rev: Optional[int]):
+    """Create a dictionary of the parameters configuring listings.
+
+    Some parameter names are using Python reserved ones. They cannot therefore
+    be directly passed to ``requests`` as ``kwargs``.
+
+    :param pagination_from: Pagination index to start from.
+    :param pagination_size: Number of results to return per page.
+    :param deprecated: Deprecation status of the resolvers to keep.
+    :param type: Type of the resolvers to keep, given as an IRI.
+    :param created_by: Identity ID of the creator of the resolvers
+        to keep, given as an IRI.
+    :param updated_by: Identity ID of the last identity which has
+        updated the resolvers to keep, given as en IRI.
+    :param rev: Revision number of the resolvers to keep.
+    :return: Dictionary of the parameters configuring listings.
+     """
+    return {
+        "from": pagination_from,
+        "size": pagination_size,
+        "deprecated": deprecated,
+        "type": type,
+        "createdBy": created_by,
+        "updatedBy": updated_by,
+        "rev": rev,
+    }
