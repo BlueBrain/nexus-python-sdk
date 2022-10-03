a0f8c186388b594d57c30c89c3cca28b79864861
Mike Gevaert <michael.gevaert@epfl.ch>
2022-10-03 17:00:44 +0200

    fix Organization::create's use of string

--- nexussdk/organizations.py
+++ nexussdk/organizations.py
@@ -15,7 +15,7 @@ class Organizations:
     def fetch(self, org_label, rev=None):
         """
         Fetch an organization.
-    
+
         :param org_label: The label of the organization
         :param rev: OPTIONAL The specific revision of the wanted organization. If not provided, will get the last
         :return: All the details of this organization, as a dictionary
@@ -31,7 +31,7 @@ class Organizations:
     def create(self, org_label, description=None):
         """
         Create a new organization.
-    
+
         :param org_label: The label of the organization. Does not allow spaces or special characters
         :param description: OPTIONAL The description of the organization
         :return: The payload from the Nexus API as a dictionary. This contains the Nexus metadata of the organization
@@ -41,7 +41,7 @@ class Organizations:
 
         data = {}
 
-        if "description" is not None:
+        if description is not None:
             data["description"] = description
         else:
             data["description"] = ""
@@ -51,7 +51,7 @@ class Organizations:
     def update(self, org, rev=None):
         """
         Update an organization. Only the field "name" can be updated (and "description" in the future).
-    
+
         :param org: Organization payload as a dictionary. This is most likely the returned value of `organisation.get(...)`
         :param rev: OPTIONAL The last revision number, to make sure the developer is aware of the latest status of
             this organization. If not provided, the `_rev` number from the `org` argument will be used.
@@ -70,7 +70,7 @@ class Organizations:
         """
         NOT WORKING
         List all the organizations.
-    
+
         :param pagination_from: OPTIONAL Index of the list to start from (default: 0)
         :param pagination_size: OPTIONAL Size of the list (default: 20)
         :return: The payload from the Nexus API as a dictionary. This contains the Nexus metadata of the organization
@@ -85,7 +85,7 @@ class Organizations:
         Deprecate an organization. Nexus does not allow deleting organizations so deprecating is the way to flag them as
         not usable anymore.
         A deprecated organization can not be modified/updated.
-    
+
         :param org_label: The label of the organization to deprecate
         :param rev: The previous revision number. To be provided to make sure the user is well aware of the details
             of the last revision of this organisation.
@@ -99,7 +99,7 @@ class Organizations:
     def events(self, last_id: Optional[str] = None):
         """
         Fetches organization related events.
-    
+
         :param last_id: ID of the last processed event, if provided, only events after
                 the event with the provided ID will be returned.
         :return: iterator of organization events
